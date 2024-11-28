013ea4ccfbbc260b3c9eefb068406397ce444584
Allego <allego.vteam@gmail.com>
2024-11-28 15:51:03 +0800

    init

---
+++ packages/practice/package/src/Practice.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Practice\Package;
+
+use App\Http\Controllers\Controller;
+
+class Practice extends Controller
+{
+    public static function Practice()
+    {
+        return 'Practice';
+    }
+}
