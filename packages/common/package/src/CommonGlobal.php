d62a2e6523eb1939e8a24f60d34adb5240f97268
Allego <allego.vteam@gmail.com>
2024-11-29 13:59:20 +0800

    Update CommonGlobal.php

--- packages/common/package/src/CommonGlobal.php
+++ packages/common/package/src/CommonGlobal.php
@@ -7,5 +7,5 @@ use App\Http\Controllers\Controller;
 class CommonGlobal extends Controller
 {
     public static $Company_SID = 0; // 儲存組織 SID
-    public static $Terminal_SID = 0; // 儲存終端 SID
+    public static $Terminal_SID = ''; // 儲存終端 SID
 }
